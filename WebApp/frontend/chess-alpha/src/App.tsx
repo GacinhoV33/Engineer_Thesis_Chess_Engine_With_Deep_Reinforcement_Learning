16b24fc2e9ac4d6ba59c94f86a65e3afb9d4c089
Daniel Gacek <Daniel.Gacek.ext@toyota-industries.eu>
2023-01-03 16:54:48 +0100

    test pipeline1

--- WebApp/frontend/chess-alpha/src/App.tsx
+++ WebApp/frontend/chess-alpha/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PlayGame from './components/PlayGame';
+import NavbarComponent from './components/NavbarComponent';
 
 function App() {
   return (
