351244f2a3cbc6d48852b76c5abc6754cad52b9d
GacinhoV33 <fgacek@student.agh.edu.pl>
2023-01-25 00:59:16 +0100

    ga-test, using ga-4

--- WebApp/src/App.tsx
+++ WebApp/src/App.tsx
@@ -5,16 +5,16 @@ import NavbarComponent from './components/Navbar/NavbarComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Thesis from './components/Thesis/Thesis';
 import Author from './components/Author/Author';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import { useEffect } from 'react';
 
 const TRACKING_ID = "G-34MLQ8JJQQ"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
   return (
     <div className="App">
       <header className="App-header">
--- WebApp/src/useAnalyticsEventTracker.tsx
+++ WebApp/src/useAnalyticsEventTracker.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const useAnalyticsEventTracker = (category: string="Blog category") => {
   const eventTracker = (action: string = "new_game", label: string = "new_game") => {
